382295816b39afc42908e8108e0620ccd7870ab0
MaxRosenblatt1 <126791778+MaxRosenblatt1@users.noreply.github.com>
2023-03-02 16:04:10 +0000

    Update displayImage.py

--- displayImage.py
+++ displayImage.py
@@ -1,22 +1,32 @@
-import matplotlib.pyplot as plt 
-  
-from astropy.visualization import astropy_mpl_style 
-  
-plt.style.use(astropy_mpl_style) 
-from astropy.io import fits 
-from astropy.utils.data import get_pkg_data_filename 
-  
-image_file = get_pkg_data_filename('star_Light_Blue_30_secs_001.fits') 
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
 
-#Change image_file to file name of whichever file you'd like to generate an image for 
-#Make sure this file is in same directory as the code. If it isn't, add the full pathname.
+from astropy.io import fits
+from astropy.utils.data import get_pkg_data_filename
+
+image_file = get_pkg_data_filename('star_Light_Blue_45_secs_034.fits')
+
+"""
+Change image_file to file name of whichever file you'd like to generate 
+an image for. Make sure this file is in same directory as the code. 
+"""
+
+fits.info(image_file)
+image_data = fits.getdata(image_file, ext=0)
+print(image_data.shape)
+plt.figure()
+
+"""
+Run this code, note down the approximate location of the star in the image,
+then uncomment lines 24 and 25 and adjust x and y limits to obtain 
+a close-up of the star.
+"""
+
+#plt.xlim([2350, 2400])
+#plt.ylim([1675, 1700])
 
 
-fits.info(image_file) 
-image_data = fits.getdata(image_file, ext=0) 
-print(image_data.shape) 
-plt.figure() 
-plt.xlim([2200, 2400]) 
-plt.ylim([1600, 1800]) 
-plt.imshow(image_data, cmap='gray') 
-plt.colorbar() 
+plt.imshow(image_data, cmap='gray')
+plt.colorbar()
+plt.show()
